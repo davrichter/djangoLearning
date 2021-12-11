8e6fbec29317dc50c6116b4d5a40ee3f370f3126
no name <mail.nice@protonmail.com>
2021-12-11 15:51:00 +0100

    articles can now be shown in a formatted or unformatted way

--- wikipedia_converter/static/wikipedia_converter/article.js
+++ wikipedia_converter/static/wikipedia_converter/article.js
@@ -1,12 +1,29 @@
 const navbarForm = document.getElementById("navbar-form")
 const downloadButton = document.createElement("button")
-downloadButton.classList.add("btn")
-downloadButton.classList.add("text-light")
-downloadButton.classList.add("bg-dark")
+downloadButton.classList.add("btn", "bg-dark", "text-light")
 downloadButton.value = "save"
 downloadButton.name = "title"
 
+const textSwitcher = document.createElement("button")
+const navbar = document.getElementById("navbar")
+textSwitcher.classList.add("btn", "btn-primary")
+textSwitcher.innerHTML = "Original"
 
 downloadButton.innerHTML = "<i class=\"bi-download\" style=\"font-size: 150%;\"></i>"
 
 navbarForm.appendChild(downloadButton)
+navbar.appendChild(textSwitcher)
+
+textSwitcher.addEventListener('click', () => {
+    let article = document.getElementById("article")
+
+    if (textSwitcher.innerHTML === "Original") {
+        article.innerHTML = articleTextOriginal
+        article.classList.remove("text-lowercase")
+        textSwitcher.innerHTML = "Formatted"
+    } else {
+        article.innerHTML = articleTextFormatted
+        article.classList.add("text-lowercase")
+        textSwitcher.innerHTML = "Original"
+    }
+})
