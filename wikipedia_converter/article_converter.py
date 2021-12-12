a11cda14a84d75228b55d9e9324fca91ff8039e1
no name <mail.nice@protonmail.com>
2021-12-12 09:15:01 +0100

    article_converter.py only removes commas, periods or semicolons if they don't come after a number

--- wikipedia_converter/article_converter.py
+++ wikipedia_converter/article_converter.py
@@ -1,6 +1,33 @@
 def article_convert(article):
-    article_modified = article.replace(",", "")
-    article_modified = article_modified.replace(".", "")
-    article_modified = article_modified.replace(";", "")
+    # article_modified = article.replace(",", "")
+    # article_modified = article_modified.replace(".", "")
+    # article_modified = article_modified.replace(";", "")
+    article_modified = ""
+
+    # remove commas, periods or semicolons only if they dont come after a number
+    for i in range(len(article)):
+        if (article[i] == "," or article[i] == "." or article[i] == ";") and i >= 1:
+            if is_number(article[i - 1]):
+                article_modified += article[i]
+
+        else:
+            article_modified += article[i]
 
     return article_modified
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
