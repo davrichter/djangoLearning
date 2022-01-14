239a90df985ba375525c17b68ad44a0cd7344633
no name <mail.nice@protonmail.com>
2022-01-14 15:37:59 +0100

    added the feature to create users

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('handwriting/', include('wikipedia_converter.urls')),
     path('', include('wikipedia_converter.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/create-user', views.get_create_user, name="getCreateUser"),
+    path('accounts/create-user-db', views.create_user, name="createUser"),
     path('admin/', admin.site.urls),
 ]
--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,3 +1,20 @@
+from django.template import loader
+from django.urls import reverse
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from wikipedia_converter.models import User
 
-user = User.objects.create_user()
+
+def get_create_user(request):
+    template = loader.get_template('registration/create_user.html')
+
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
+
+def create_user(request):
+    user = User.objects.create_user(username=request.POST["username"], password=request.POST["password"])
+    user.save()
+
+    return HttpResponseRedirect("login")
